53298f967f2fed415ab8b05affd7703f27fb74cf
andac-demir <andacdemir@hotmail.com>
2018-09-22 17:12:56 -0400

    cifar10 load and process

---
+++ data_processing.py
@@ -0,0 +1,50 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+def load_cifar10():
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), 
+                                                         (0.5, 0.5, 0.5))])
+    trainset = torchvision.datasets.CIFAR10(root='./CIFAR10', train=True,
+                                            download=True, transform=transform)
+    testset = torchvision.datasets.CIFAR10(root='./CIFAR10', train=False,
+                                           download=True, transform=transform)
+    return trainset, testset
+
+def batch_data(trainset, testset, batch_size=4):
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=2)
+    return trainloader, testloader
+
+def test_loading(trainloader, classes):
+    def imshow(img):
+        img = img / 2 + 0.5     # unnormalize
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+    # get some random training images
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+    # print labels
+    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+def main():
+    trainset, testset = load_cifar10()
+    trainloader, testloader = batch_data(trainset, testset)
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    test_loading(trainloader, classes)
+
+if __name__ == '__main__':
+    main()
+
+
+

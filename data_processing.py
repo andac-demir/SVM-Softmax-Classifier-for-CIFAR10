2c7b45e01a62870194acfab05ca5bc69c90d1e5d
andac-demir <andacdemir@hotmail.com>
2018-09-22 17:20:47 -0400

    cifar10 load and process

--- data_processing.py
+++ data_processing.py
@@ -26,15 +26,16 @@ def test_loading(trainloader, classes):
         img = img / 2 + 0.5     # unnormalize
         npimg = img.numpy()
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.show()
 
     # get some random training images
     dataiter = iter(trainloader)
     images, labels = dataiter.next()
 
-    # show images
-    imshow(torchvision.utils.make_grid(images))
     # print labels
     print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    # show images
+    imshow(torchvision.utils.make_grid(images))
 
 def main():
     trainset, testset = load_cifar10()

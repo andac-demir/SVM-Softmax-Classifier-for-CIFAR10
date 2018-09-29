6ffa9b916c7182469d387982af8d1c80e83fae51
andac-demir <andacdemir@hotmail.com>
2018-09-28 23:54:30 -0400

    trained model saved.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import data_processing as dp
 from model import SoftmaxClassifier
 import model
 
+def save_model(net):
+    torch.save(net.state_dict(), f="../TrainedModels/" + 
+                              "sofmax_classifier.model")
+                                     
 def main():
     # This loads the dataset and partitions it into batches:
     trainset, testset = dp.load_cifar10()
@@ -9,10 +13,14 @@ def main():
     # Loads the model and the training/testing functions:
     net = SoftmaxClassifier()
     criterion, optimizer, epochs = model.set_optimization(net)
-    # TODO: 
-    for epoch in epochs:
+    
+    # Print the train and test accuracy after every epoch:
+    for epoch in range(epochs):
         model.train(net, trainloader, criterion, optimizer, epoch)
         model.test(net, testloader, epoch)
+    
+    # Save the model:
+    save_model(net)
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -29,31 +29,30 @@ class SoftmaxClassifier(nn.Module):
 def set_optimization(model):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    epochs = 2
+    epochs = 1
     return criterion, optimizer, epochs
 
 def train(model, trainloader, criterion, optimizer, epoch):
-    for epoch in range(epochs):  
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs
-            inputs, labels = data
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
+        # zero the parameter gradients
+        optimizer.zero_grad()
 
-            # forward + backward + optimize
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+        # forward + backward + optimize
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
 
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
                     (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
+            running_loss = 0.0
 
     print('Finished Training')
 
